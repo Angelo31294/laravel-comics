630f358301cf5c87742f4fbc39798c3a2ca0f075
Angelo31294 <luongoangelo1@gmail.com>
2023-01-15 18:24:35 +0100

    Struttura base

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('page-title')
+    Comics
+@endsection
+
+@section('page-content')
+    <section>
+        <h1>lista prodotti</h1>
+    </section>    
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,37 @@
+<header>
+    <div class="text-bg-primary">
+        <div class="container">
+            <ul class="d-flex list-unstyled justify-content-end">
+                <li class="item"> DC POWER VISA </li>
+                <li class="item"> ADDITIONAL DC SITE <i class="fa-solid fa-sort-down"></i></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="container">
+
+        <nav class="d-flex justify-content-between align-items-center">
+
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" class="img-fluid" alt="Logo DC">
+
+            <ul class="d-flex list-unstyled">
+                <li><a class="menu-item" href="#"><strong>CHARACTERS</strong></a></li>
+                <li><a class="menu-item menu-item--active" href="#"><strong>COMICS</strong></a></li>
+                <li><a class="menu-item" href="#"><strong>MOVIES</strong></a></li>
+                <li><a class=" menu-item" href="#"><strong>TV</strong></a></li>
+                <li><a class=" menu-item" href="#"><strong>GAMES</strong></a></li>
+                <li><a class=" menu-item" href="#"><strong>COLLECTIBLES</strong></a></li>
+                <li><a class=" menu-item" href="#"><strong>VIDEO</strong></a></li>
+                <li><a class=" menu-item" href="#"><strong>FANS</strong></a></li>
+                <li><a class=" menu-item" href="#"><strong>NEWS</strong></a></li>
+                <li><a class=" menu-item" href="#"><strong>SHOP</strong></a></li>
+            </ul>
+
+            <div>
+                <input type="text" class="" placeholder="Search" aria-label="Recipient's username" aria-describedby="button-addon2">
+                <button class="btn" type="button" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+        </nav>
+
+    </div>
+</header>
